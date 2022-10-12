df7f37cb0e5d590044a458ceec26e305ba3d3a68
chanyoung Hong <cksdud9883@naver.com>
2022-10-12 11:20:16 +0900

    [update] change class name

--- src/main/java/com/cosmost/project/comment/infrastructure/repository/CourseReviewEntityRepository.java
+++ src/main/java/com/cosmost/project/comment/infrastructure/repository/CourseReviewEntityRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseReviewRepository extends JpaRepository<CourseReviewEntity, Long> {
+public interface CourseReviewEntityRepository extends JpaRepository<CourseReviewEntity, Long> {
 }

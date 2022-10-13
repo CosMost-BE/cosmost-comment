35af5700f526ed4c9a361de4894675c4cda19d1b
ChanYoungHong <cksdud9883@naver.com>
2022-10-14 02:42:43 +0900

    [update] add readCourseDetailReviews in CourseReviewService

--- src/main/java/com/cosmost/project/comment/service/CourseReviewService.java
+++ src/main/java/com/cosmost/project/comment/service/CourseReviewService.java
@@ -2,6 +2,7 @@ package com.cosmost.project.comment.service;
 
 import com.cosmost.project.comment.infrastructure.entity.CourseReviewEntity;
 import com.cosmost.project.comment.infrastructure.entity.CourseReviewStatus;
+import com.cosmost.project.comment.model.CourseReview;
 import com.cosmost.project.comment.requestbody.CreateCourseReviewRequest;
 import com.cosmost.project.comment.requestbody.UpdateCourseReviewRequest;
 import com.cosmost.project.comment.view.CourseReviewView;
@@ -13,7 +14,7 @@ public interface CourseReviewService {
     // create, read, update, delete
     Long createCourseReviews(CreateCourseReviewRequest createCourseReviewRequest);
     List<CourseReviewView> readMyCourseReviews();
-
+    List<CourseReviewView> readCourseDetailReviews();
     void updateCourseReviews(Long id, UpdateCourseReviewRequest updateCourseReviewRequest);
     void deleteCourseReview(Long id);
 

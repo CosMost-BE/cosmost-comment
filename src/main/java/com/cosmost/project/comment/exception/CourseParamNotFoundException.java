4679a339e37730e37e9954a5627f8226265139f2
chanyoung Hong <cksdud9883@naver.com>
2022-10-13 16:16:58 +0900

    [update] change Message content

--- src/main/java/com/cosmost/project/comment/exception/CourseParamNotFoundException.java
+++ src/main/java/com/cosmost/project/comment/exception/CourseParamNotFoundException.java
@@ -4,7 +4,7 @@ import java.util.NoSuchElementException;
 
 public class CourseParamNotFoundException extends NoSuchElementException {
 
-    private static final String MESSAGE = "입력값된 쿼리값이 옳바르지 않습니다.";
+    private static final String MESSAGE = "입력된 쿼리값이 올바르지 않습니다.";
 
     public  CourseParamNotFoundException() {
         super(MESSAGE);

c87c54a12e32934d9540b2d540a81fd77202cad3
chanyoung Hong <cksdud9883@naver.com>
2022-10-13 14:31:48 +0900

    [delete] delete constructor

--- src/main/java/com/cosmost/project/comment/view/CourseReviewView.java
+++ src/main/java/com/cosmost/project/comment/view/CourseReviewView.java
@@ -1,13 +1,14 @@
 package com.cosmost.project.comment.view;
 
 import com.cosmost.project.comment.infrastructure.entity.CourseReviewStatus;
-import com.cosmost.project.comment.model.CourseReview;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseReviewView {
 
@@ -17,14 +18,5 @@ public class CourseReviewView {
     private final CourseReviewStatus courseReviewStatus;
     private final String courseReviewContent;
     private final float rate;
-
-    public CourseReviewView(CourseReview courseReview) {
-        this.id = courseReview.getId();
-        this.courseId = courseReview.getCourseId();
-        this.reviewerId = courseReview.getReviewerId();
-        this.courseReviewStatus = courseReview.getCourseReviewStatus();
-        this.courseReviewContent = courseReview.getCourseReviewContent();
-        this.rate = courseReview.getRate();
-    }
 
 }

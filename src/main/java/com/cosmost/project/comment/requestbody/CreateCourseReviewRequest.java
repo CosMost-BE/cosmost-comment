1cdfd6391ed25879ce537ca047e94cceb81f9315
chanyoung Hong <cksdud9883@naver.com>
2022-10-14 21:10:45 +0900

    [delete] delete Data annotation

--- src/main/java/com/cosmost/project/comment/requestbody/CreateCourseReviewRequest.java
+++ src/main/java/com/cosmost/project/comment/requestbody/CreateCourseReviewRequest.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Data
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,7 +23,7 @@ public class CreateCourseReviewRequest {
     private CourseReviewStatus courseReviewStatus;
     private Long reviewerId;
 
-    @Size(max = 100, message = "상품평은 ??자를 넘을 수 없습니다.")
+    @Size(max = 100, message = "상품평은 100자를 넘을 수 없습니다.")
     private String courseReviewContent;
 
     @Min(value = 1, message = "별점은 1이상 5이하여야 합니다.")

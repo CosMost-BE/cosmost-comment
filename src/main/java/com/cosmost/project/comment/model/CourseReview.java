56d17c08d5efb19b285552fec37f35f361223e57
chanyoung Hong <cksdud9883@naver.com>
2022-11-01 19:40:47 +0900

    [update] add annotation

--- src/main/java/com/cosmost/project/comment/model/CourseReview.java
+++ src/main/java/com/cosmost/project/comment/model/CourseReview.java
@@ -2,17 +2,17 @@ package com.cosmost.project.comment.model;
 
 import com.cosmost.project.comment.infrastructure.entity.CourseReviewEntity;
 import com.cosmost.project.comment.infrastructure.entity.CourseReviewStatus;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @ToString
+@Builder
+@AllArgsConstructor
 public class CourseReview {
 
     private Long id;
-
     private Long courseId;
 
     private Long reviewerId;
@@ -20,7 +20,6 @@ public class CourseReview {
     private String courseReviewContent;
 
     private CourseReviewStatus courseReviewStatus;
-
     private Integer rate;
 
     public CourseReview(CourseReviewEntity entity) {

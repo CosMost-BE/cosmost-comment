0a4bf8d0d9d3c4f552cd0a4031cd179467261bff
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 10:59:37 +0900

    [update] refactor CourseDetailReviewView

--- src/main/java/com/cosmost/project/comment/view/CourseDetailReviewView.java
+++ src/main/java/com/cosmost/project/comment/view/CourseDetailReviewView.java
@@ -14,21 +14,10 @@ import java.util.List;
 @AllArgsConstructor
 public class CourseDetailReviewView {
 
-    private Long id;
     private Long courseId;
-    private double rate;
+    private Long courseReviewCnt;
 
     String[] rateAllTypeList;
-    ArrayList<ArrayList<Integer>> integerList;
     List<CourseReviewEntity> courseReviewList;
-
-    public CourseDetailReviewView(CourseReviewEntity courseReview) {
-        this.id = courseReview.getId();
-        this.rate = courseReview.getRate();
-        this.courseId = courseReview.getCourseId();
-        this.integerList = getIntegerList();
-        this.rateAllTypeList = getRateAllTypeList();
-        this.courseReviewList = getCourseReviewList();
-    }
 
 }

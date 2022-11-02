c0abf9995198c8301509ea619fcb5f8d97c9b9c3
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 11:13:57 +0900

    [update] code refactoring

--- src/main/java/com/cosmost/project/comment/controller/ViewCourseReviewController.java
+++ src/main/java/com/cosmost/project/comment/controller/ViewCourseReviewController.java
@@ -1,5 +1,6 @@
 package com.cosmost.project.comment.controller;
 
+import com.cosmost.project.comment.exception.CourseNotFoundException;
 import com.cosmost.project.comment.service.ViewCourseReviewService;
 import com.cosmost.project.comment.view.ViewCourseRate;
 import lombok.extern.slf4j.Slf4j;
@@ -25,12 +26,11 @@ public class ViewCourseReviewController {
 
     @GetMapping("")
     public ResponseEntity<List<ViewCourseRate>> read(@RequestParam(value = "rate", required = false) String rate,
-                                                     @RequestParam(value = "courseId", required = false) Long courseId) {
+                                                     @RequestParam(value = "course", required = false) Long course) {
 
-        log.info(String.valueOf(courseId));
-//        if (rate.equals("average") && !(courseId.equals(null))) {
-
-
-        return ResponseEntity.ok().body(viewCourseReviewService.readCourseAvg(courseId));
+        if(rate.equals("average") && !(course.equals(null))) {
+            return ResponseEntity.ok().body(viewCourseReviewService.readCourseAvg(course));
+        }
+        return null;
     }
 }

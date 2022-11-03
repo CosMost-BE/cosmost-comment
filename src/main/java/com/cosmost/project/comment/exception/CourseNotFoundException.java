5ec01ca88ec2d725346e1af54a9cf02625a94bbe
chanyoung Hong <cksdud9883@naver.com>
2022-11-03 21:15:29 +0900

    [update] add CourseNotFoundException

--- src/main/java/com/cosmost/project/comment/exception/CourseNotFoundException.java
+++ src/main/java/com/cosmost/project/comment/exception/CourseNotFoundException.java
@@ -1,7 +1,7 @@
 package com.cosmost.project.comment.exception;
 
 public class CourseNotFoundException extends RuntimeException {
-    private static final String MESSAGE = "코스ID가 존재하지 않습니다";
+    private static final String MESSAGE = "해당 코스의 리뷰가 존재하지 않습니다";
 
     public  CourseNotFoundException() {
         super(MESSAGE);

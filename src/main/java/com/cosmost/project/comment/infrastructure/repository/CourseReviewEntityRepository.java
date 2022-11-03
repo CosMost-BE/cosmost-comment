2200ac251309ee01e5d81e05a0b5986fd24de6b4
chanyoung Hong <cksdud9883@naver.com>
2022-11-03 21:16:30 +0900

    [update] add findByReviewerIdAndCourseId method

--- src/main/java/com/cosmost/project/comment/infrastructure/repository/CourseReviewEntityRepository.java
+++ src/main/java/com/cosmost/project/comment/infrastructure/repository/CourseReviewEntityRepository.java
@@ -11,6 +11,10 @@ import java.util.Optional;
 public interface CourseReviewEntityRepository extends JpaRepository<CourseReviewEntity, Long> {
 
     List<CourseReviewEntity> findAllByReviewerId(Long reviewerId);
+
     List<CourseReviewEntity> findAllByCourseId(Long courseId);
-    List<CourseReviewEntity> findByCourseId(Long cousreId);
+
+    List<CourseReviewEntity> findByCourseId(Long courseId);
+
+    List<CourseReviewEntity> findByReviewerIdAndCourseId(Long reviewerId, Long courseId);
 }

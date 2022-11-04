25a94330267761e084e7e963431449c5c08db5e4
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 21:23:17 +0900

    [update] add findByReportId method

--- src/main/java/com/cosmost/project/comment/infrastructure/repository/ReportAnswerEntityRepository.java
+++ src/main/java/com/cosmost/project/comment/infrastructure/repository/ReportAnswerEntityRepository.java
@@ -2,10 +2,11 @@ package com.cosmost.project.comment.infrastructure.repository;
 
 
 import com.cosmost.project.comment.infrastructure.entity.ReportAnswerEntity;
+import com.cosmost.project.comment.model.ReportAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportAnswerEntityRepository extends JpaRepository<ReportAnswerEntity, Long> {
-
+    ReportAnswer findByReportId(Long reportId);
 }

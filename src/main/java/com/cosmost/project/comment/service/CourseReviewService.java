5c2413b4c63f9967d463f7e7d6cf09358c72df32
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 18:16:22 +0900

    [update] add pageable

--- src/main/java/com/cosmost/project/comment/service/CourseReviewService.java
+++ src/main/java/com/cosmost/project/comment/service/CourseReviewService.java
@@ -5,7 +5,9 @@ import com.cosmost.project.comment.infrastructure.entity.CourseReviewStatus;
 import com.cosmost.project.comment.model.CourseReview;
 import com.cosmost.project.comment.requestbody.CreateCourseReviewRequest;
 import com.cosmost.project.comment.requestbody.UpdateCourseReviewRequest;
+import com.cosmost.project.comment.responsebody.ReadMyCourseReviewsResponse;
 import com.cosmost.project.comment.view.CourseDetailReviewView;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -13,8 +15,8 @@ public interface CourseReviewService {
 
     // create, read, update, delete
     CourseReview createCourseReviews(CreateCourseReviewRequest createCourseReviewRequest);
-    List<CourseReview> readMyCourseReviews();
-    List<CourseDetailReviewView> readCourseDetailReviews();
+    List<ReadMyCourseReviewsResponse> readMyCourseReviews(Pageable pageable);
+    List<CourseDetailReviewView> readCourseDetailReviews(Pageable pageable);
     void updateCourseReviews(Long id, UpdateCourseReviewRequest updateCourseReviewRequest);
     void deleteCourseReview(Long id);
 

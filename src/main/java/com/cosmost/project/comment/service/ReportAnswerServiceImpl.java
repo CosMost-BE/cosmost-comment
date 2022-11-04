60e31860327412d3b7f13c1628ee56b1d0105f76
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 21:23:34 +0900

    [update] add readMyReport method

--- src/main/java/com/cosmost/project/comment/service/ReportAnswerServiceImpl.java
+++ src/main/java/com/cosmost/project/comment/service/ReportAnswerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cosmost.project.comment.service;
 
+import com.cosmost.project.comment.exception.ReportAnswerNotFoundException;
 import com.cosmost.project.comment.infrastructure.entity.ReportAnswerEntity;
 import com.cosmost.project.comment.infrastructure.repository.ReportAnswerEntityRepository;
+import com.cosmost.project.comment.model.ReportAnswer;
 import com.cosmost.project.comment.requestbody.CreateCourseReviewRequest;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -44,5 +47,26 @@ public class ReportAnswerServiceImpl implements ReportAnswerService {
         reportAnswerEntityRepository.save(reportAnswer);
     }
 
+    @Override
+    public ReportAnswer readMyReport() {
+        HttpServletRequest request = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization");
+        Long reporterId = Long.parseLong(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
 
+        Optional<ReportAnswer> reportAnswer =
+                Optional.ofNullable(reportAnswerEntityRepository.findByReportId(reporterId));
+
+        if (reportAnswer.isPresent()) {
+            return ReportAnswer.builder()
+                    .reportId(reporterId)
+                    .answerPersonId(reportAnswer.get().getAnswerPersonId())
+                    .reportTitle(reportAnswer.get().getReportTitle())
+                    .reportContent(reportAnswer.get().getReportContent())
+                    .build();
+        } else {
+            throw new ReportAnswerNotFoundException();
+        }
+    }
 }
+
